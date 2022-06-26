9eb24705f2f157e87c6d7e3f851edd0850790633
Dave Pitts <dgapitts@gmail.com>
2022-06-26 23:12:27 +0200

    initial vagrant c7 provisioning and setup files plus basic usage notes

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# vagrant-c7-aws-terraform-ansible
+## vagrant-c7-aws-terraform-ansible
+
+Running terraform with aws provider in a centos7 VM
+* After running vagrant up
+* I manually setup a General Admin User and then run `aws configure` (as vagrant user)
+* Following [DevOpsDays Amsterdam exercises](https://gitlab.com/mvandongen/tfworks)
---
+++ master.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+source /vagrant/provision_base.sh
+yum install -y unzip
+
+yum install -y yum-utils
+yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
+yum -y install terraform
+terraform version
+
+yum install python3-pip -y
+pip3 install awscli
+
+# curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+# unzip awscliv2.zip
+#  ./aws/install
+# aws --version
+/usr/local/bin/aws --version
+
+yum install epel-release -y
+
